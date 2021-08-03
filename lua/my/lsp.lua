75ddf0d4c69eea9d3300c3ad41d206bfa60d3c62
Pasi Bergman <pasi.bergman@iki.fi>
2021-08-03 14:33:33 +0300

    Check for prettier configuration in the CWD project when selecting JS formatters

--- lua/my/lsp.lua
+++ lua/my/lsp.lua
@@ -1,28 +1,86 @@
 --
--- Setup formatters for JavaScript family
+-- Set preferred linters and formatters
 --
-lvim.lang.javascript.formatters = {
+local lua_formatter = "stylua"
+local prettier = "prettier"
+
+local js_linter = "eslint_d"
+local css_linter = "stylelint" -- stylelint not supported yet
+local sh_linter = "shellcheck"
+
+local use_eslint_fix = true
+--
+-- Does package.json file contain speficied property (e.g. "prettier")?
+--
+local in_package_json = function(field)
+  print "function in_package_json"
+  if vim.fn.filereadable "package.json" ~= 0 then
+    local package_json = vim.fn.json_decode(vim.fn.readfile "package.json")
+    return (package_json[field] ~= nil)
+  end
+  return false
+end
+--
+-- Does the current working directory (project) have a Prettier configuration?
+--
+local project_has_prettier_config = function()
+  return (vim.fn.glob ".prettierrc*" ~= "" or in_package_json "prettier")
+end
+--
+-- Get JavaScript family formatters based on configuration
+--
+local get_js_formatters = function()
+  local formatters = {}
+  local eslint_fix = {
+    exe = js_linter,
+    args = {},
+  }
+  if js_linter == "eslint_d" and use_eslint_fix == true then
+    table.insert(formatters, eslint_fix)
+  end
+  if project_has_prettier_config() == true then
+    local prettier_fmt = {
+      exe = prettier,
+      args = {},
+    }
+    table.insert(formatters, prettier_fmt)
+  end
+  return formatters
+end
+--
+-- Lua formatter
+--
+lvim.lang.lua.formatters = {
   {
-    exe = "eslint_d",
-    args = lvim.lang.javascript.formatters[1].args,
+    exe = lua_formatter,
+    args = {},
   },
 }
+--
+-- Setup formatters for JavaScript family
+--
+lvim.lang.javascript.formatters = get_js_formatters()
 lvim.lang.typescript.formatters = lvim.lang.javascript.formatters
 lvim.lang.javascriptreact.formatters = lvim.lang.javascript.formatters
 lvim.lang.typescriptreact.formatters = lvim.lang.javascript.formatters
 lvim.lang.vue.formatters = lvim.lang.javascript.formatters
+lvim.lang.css.formatters = { { exe = prettier, args = {} } }
 --
 -- Setup linters for JavaScript family
 --
-lvim.lang.javascript.linters = { { exe = "eslint_d" } }
+lvim.lang.javascript.linters = { { exe = js_linter } }
 lvim.lang.typescript.linters = lvim.lang.javascript.linters
 lvim.lang.javascriptreact.linters = lvim.lang.javascript.linters
 lvim.lang.typescriptreact.linters = lvim.lang.javascript.linters
 lvim.lang.vue.linters = lvim.lang.javascript.linters
 --
+-- Linter for css/sass/scss/
+--
+lvim.lang.css.linters = { { exe = css_linter } }
+--
 -- Setup shell/bash/zsh linter
 --
-lvim.lang.sh.linters = { { exe = "shellcheck" } }
+lvim.lang.sh.linters = { { exe = sh_linter } }
 
 -- generic LSP settings
 -- you can set a custom on_attach function that will be used for all the language servers
