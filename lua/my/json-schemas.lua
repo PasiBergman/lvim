83bdbf57b3d73ead43312db5caaab4284cf7d30f
Pasi Bergman <pasi.bergman@iki.fi>
2021-08-05 16:07:28 +0300

    Stylua formatted files

--- lua/my/json-schemas.lua
+++ lua/my/json-schemas.lua
@@ -51,48 +51,48 @@ M.setup = function()
     {
       description = "Azure pipelines v1.174.2",
       fileMatch = { "azure-pipelines.yml" },
-      url = "https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/v1.174.2/service-schema.json"
+      url = "https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/v1.174.2/service-schema.json",
     },
     {
       description = "Bootstrap-loader config",
       fileMatch = { ".bootstraprc" },
-      url = "https://json.schemastore.org/bootstraprc.json"
+      url = "https://json.schemastore.org/bootstraprc.json",
     },
     {
       description = "Azure Functions function.json file",
       fileMatch = { "functions.json" },
-      url = "https://json.schemastore.org/function.json"
+      url = "https://json.schemastore.org/function.json",
     },
     {
       description = "OmniSharp formatting config",
       fileMatch = { "omnisharp.json" },
-      url = "https://json.schemastore.org/omnisharp.json"
+      url = "https://json.schemastore.org/omnisharp.json",
     },
     {
       description = "OpenAPI schema v3.1",
       fileMatch = { "openapi.json" },
-      url = "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"
+      url = "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json",
     },
     {
       description = "Swagger 2.0 API schema",
       fileMatch = { "swagger.json", "swagger.yml" },
-      url = "https://json.schemastore.org/swagger-2.0.json"
+      url = "https://json.schemastore.org/swagger-2.0.json",
     },
     {
       description = "xUnit runner cofngiguration v2.3",
       filematch = { "xunit.json" },
-      url = "https://json.schemastore.org/xunit.runner.schema.json"
+      url = "https://json.schemastore.org/xunit.runner.schema.json",
     },
     {
       description = "Docker Compose configuration",
       filematch = { "compose.yml" },
-      url = "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"
+      url = "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json",
     },
     {
       description = "Markdownlint config file",
       filematch = { "markdownlint.yml" },
-      url = "https://json.schemastore.org/markdownlint.json"
-    }
+      url = "https://json.schemastore.org/markdownlint.json",
+    },
   }
 
   local function extend(tab1, tab2)
--- lua/my/telescope.lua
+++ lua/my/telescope.lua
@@ -3,8 +3,8 @@
 -- Requires git-delta, e.g. on macOS
 -- brew install git-delta
 
-local previewers = require('telescope.previewers')
-local builtin = require('telescope.builtin')
+local previewers = require "telescope.previewers"
+local builtin = require "telescope.builtin"
 -- local conf = require('telescope.config')
 
 local E = {}
@@ -14,15 +14,14 @@ local delta = previewers.new_termopen_previewer {
     -- this is for status
     -- You can get the AM things in entry.status. So we are displaying file if entry.status == '??' or 'A '
     -- just do an if and return a different command
-    if entry.status == '??' or 'A ' then
-      return { 'git', '-c', 'core.pager=delta', '-c', 'delta.side-by-side=false', 'diff', entry.value }
+    if entry.status == "??" or "A " then
+      return { "git", "-c", "core.pager=delta", "-c", "delta.side-by-side=false", "diff", entry.value }
     end
 
     -- note we can't use pipes
     -- this command is for git_commits and git_bcommits
-    return { 'git', '-c', 'core.pager=delta', '-c', 'delta.side-by-side=false', 'diff', entry.value .. '^!' }
-
-  end
+    return { "git", "-c", "core.pager=delta", "-c", "delta.side-by-side=false", "diff", entry.value .. "^!" }
+  end,
 }
 
 E.my_git_commits = function(opts)
