95b3327d9edbeadf1663bf33f5b5c85de6042a68
Pasi Bergman <pasi.bergman@iki.fi>
2021-08-05 16:07:40 +0300

    Add ZenMode plugin

--- lua/my/plugins.lua
+++ lua/my/plugins.lua
@@ -63,4 +63,45 @@ lvim.plugins = {
     after = "nvim-compe",
     event = "InsertEnter",
   },
+  {
+    "folke/zen-mode.nvim",
+    config = function()
+      require("zen-mode").setup {
+        window = {
+          backdrop = 1,
+          height = 0.9, -- height of the Zen window
+          width = 0.85,
+          options = {
+            signcolumn = "no", -- disable signcolumn
+            number = false, -- disable number column
+            relativenumber = false, -- disable relative numbers
+            -- cursorline = false, -- disable cursorline
+            -- cursorcolumn = false, -- disable cursor column
+            -- foldcolumn = "0", -- disable fold column
+            -- list = false, -- disable whitespace characters
+          },
+        },
+        plugins = {
+          kitty = {
+            enabled = true,
+            font = "+4",
+          },
+        },
+      }
+    end,
+    event = "BufRead",
+  },
+  --[[
+  {
+    "folke/twilight.nvim",
+    config = function()
+      require("twilight").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end,
+    event = "BufRead",
+  },
+  --]]
 }
