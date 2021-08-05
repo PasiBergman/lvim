7d0728365a13d152a431ae122445ecab290e343b
Pasi Bergman <pasi.bergman@iki.fi>
2021-08-05 16:06:21 +0300

    Timoutlen, kitty

--- lua/my/general.lua
+++ lua/my/general.lua
@@ -6,8 +6,15 @@ lvim.colorscheme = "spacegray"
 -- keymappings
 lvim.leader = "space"
 
+-- keymap timeout
+vim.cmd [[set timeoutlen=250]]
+
 -- debug
 lvim.debug = true
 
 -- Set session directory
 vim.g.auto_session_root_dir = vim.fn.expand "$HOME/.cache/nvim/sessions"
+
+-- kitty
+-- https://sw.kovidgoyal.net/kitty/faq/#some-special-symbols-are-rendered-small-truncated-in-kitty
+vim.cmd [[let &t_ut='']]
