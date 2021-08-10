927ba2af551345486c3e5a4316bf3ab2ad294792
Pasi Bergman <pasi.bergman@iki.fi>
2021-08-10 11:14:16 +0300

    Set .tmux filetype. Change BufferVisibleMode and BufferCurrentMod highlight

--- lua/my/autocommands.lua
+++ lua/my/autocommands.lua
@@ -1,5 +1,6 @@
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 lvim.autocommands.custom_groups = {
   { "BufEnter", "*", "lua require'my.highlights'" },
+  { "BufEnter", "*.tmux", "set filetype=sh" },
   { "InsertEnter", "*", "highlight PmenuSel ctermfg=242 ctermbg=0 gui=bold guifg=#88c0d0 guibg=#8091b2" },
 }
--- lua/my/highlights.lua
+++ lua/my/highlights.lua
@@ -1,6 +1,13 @@
+--
 -- Collection of highlights changes
+--
+-- LSP diagnostics virtual texts
 vim.cmd [[highlight LspDiagnosticsUnderlineInformation gui=undercurl]]
 vim.cmd [[highlight LspDiagnosticsUnderlineHint gui=undercurl]]
 vim.cmd [[highlight LspDiagnosticsUnderlineWarning gui=undercurl]]
 vim.cmd [[highlight LspDiagnosticsUnderlineError gui=undercurl]]
+-- Dropdown menu
 vim.cmd [[highlight PmenuSel ctermfg=242 ctermbg=0 gui=bold guifg=#88c0d0 guibg=#8091b2]]
+-- Bufferline
+vim.cmd [[highlight BufferVisibleMod guifg=#88c0d0 guibg=#4c566a]]
+vim.cmd [[highlight BufferCurrentMod guifg=#88c0d0 guibg=#2e3440 gui=bold]]
