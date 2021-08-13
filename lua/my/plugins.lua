14d45470476244081e7034ecf3f96d0748e8b694
Pasi Bergman <pasi.bergman@iki.fi>
2021-08-13 09:58:36 +0300

    Add gitlinker plugin

--- lua/my/plugins.lua
+++ lua/my/plugins.lua
@@ -112,6 +112,26 @@ lvim.plugins = {
     end,
     event = "BufRead",
   },
+  {
+    "ruifm/gitlinker.nvim",
+    event = "BufRead",
+    config = function()
+      require("gitlinker").setup {
+        opts = {
+          -- remote = 'github', -- force the use of a specific remote
+          -- adds current line nr in the url for normal mode
+          add_current_line_on_normal_mode = true,
+          -- callback for what to do with the url
+          action_callback = require("gitlinker.actions").open_in_browser,
+          -- print the url after performing the action
+          print_url = false,
+          -- mapping to call url generation
+          mappings = "<leader>gy",
+        },
+      }
+    end,
+    requires = "nvim-lua/plenary.nvim",
+  },
   --[[
   {
     "folke/twilight.nvim",
