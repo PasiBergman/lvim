2a543d36c604063882dba9be7a18557e086aaa77
Pasi Bergman <pasi.bergman@iki.fi>
2021-08-16 21:10:39 +0300

    Add react filetypes to emmet_ls config.

--- lua/my/emmet.lua
+++ lua/my/emmet.lua
@@ -10,6 +10,15 @@ local configs = require "lspconfig/configs"
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
+--[[
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+  properties = {
+    "documentation",
+    "detail",
+    "additionalTextEdits",
+  },
+}
+--]]
 
 if not lspconfig.emmet_ls then
   configs.emmet_ls = {
@@ -23,6 +32,7 @@ if not lspconfig.emmet_ls then
         "haml",
         "hbs",
         "html",
+        "javascriptreact",
         "less",
         "php",
         "postcss",
@@ -30,6 +40,7 @@ if not lspconfig.emmet_ls then
         "sass",
         "scss",
         "svelte",
+        "typescriptreact",
         "vue",
       },
       root_dir = function(_)
