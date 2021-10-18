716e1c3a4eeb0951019240070b40933ceb4029a5
Pasi Bergman <pasi.bergman@iki.fi>
2021-10-18 18:10:47 +0300

    Disable tsserver and jsonls formatting capabilities

--- lua/my/lsp.lua
+++ lua/my/lsp.lua
@@ -136,3 +136,14 @@ end
 
 formatters.setup(formatters_table)
 linters.setup(linters_table)
+
+-- Disable formatting capability of tsserver and jsonls
+-- as we use prettier and/or eslint_d to format/fix
+lvim.lsp.on_attach_callback = function(client, _)
+  if client.name ~= "tsserver" and client.name ~= "jsonls" then
+    return
+  end
+
+  client.resolved_capabilities.document_formatting = false
+  client.resolved_capabilities.document_range_formatting = false
+end
