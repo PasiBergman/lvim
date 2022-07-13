35221ddf5d4c37b501013c098b017fc9f86cdb48
Pasi Bergman <pasi.bergman@hiq.fi>
2022-07-13 22:04:02 +0300

    Fix treesitter ensure_installed.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,6 @@
 local Treesitter = {}
 
 Treesitter.config = function()
-  if not lvim.builtin.treesitter.active then
-    return
-  end
-
   -- if you don't want all the parsers change this to a table of the ones you want
   lvim.builtin.treesitter.ensure_installed = {
     "bash",
