0364ba4ff3c5046594a88a13f7860909afba7954
Pasi Bergman <pasi.bergman@hiq.fi>
2022-11-21 10:41:10 +0200

    Add NvimTree highlight changes for Nordic

--- after/plugin/nordic.lua
+++ after/plugin/nordic.lua
@@ -3,4 +3,6 @@
 if lvim.colorscheme == "nordic" then
   vim.api.nvim_set_hl(0, "LspDiagnosticsDefaultError", { foreground = "#CC5A1B" }) -- Was #bf616a
   vim.api.nvim_set_hl(0, "DiagnosticError", { foreground = "#CC5A1B" }) -- Was #bf616a
+  vim.api.nvim_set_hl(0, "NvimTreeNormalNC", { foreground = "#a9b1d6", background = "#2e3440" })
+  vim.api.nvim_set_hl(0, "NvimTreeWinSeparator", { foreground = "#16161e", background = "#2e3440" })
 end
