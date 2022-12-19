085ad5512f3b3d600500fb4e86de2b9e03c5b429
Pasi Bergman <pasi.bergman@hiq.fi>
2022-12-19 17:32:21 +0200

    Fix hop configuration.

--- lua/user/plugins/hop.lua
+++ lua/user/plugins/hop.lua
@@ -10,25 +10,25 @@ M.config = function()
   local directions = require("hop.hint").HintDirection
 
   -- Override the default f and F
-  vim.keymap.set("", "f", function()
+  vim.keymap.set("n", "f", function()
     hop.hint_char1 { direction = directions.AFTER_CURSOR, current_line_only = true }
   end, { remap = true })
-  vim.keymap.set("", "F", function()
+  vim.keymap.set("n", "F", function()
     hop.hint_char1 { direction = directions.BEFORE_CURSOR, current_line_only = true }
   end, { remap = true })
-  vim.keymap.set("", "t", function()
+  vim.keymap.set("n", "t", function()
     hop.hint_char1 { direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }
   end, { remap = true })
-  vim.keymap.set("", "T", function()
+  vim.keymap.set("n", "T", function()
     hop.hint_char1 { direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }
   end, { remap = true })
 
   -- Skandinavian keyboard has Ö on the right side of letter L
-  vim.keymap.set("", "ö", function()
+  vim.keymap.set("n", "ö", function()
     hop.hint_char2 { direction = directions.AFTER_CURSOR, current_line_only = false }
   end, { remap = true })
-  vim.keymap.set("", "Ö", function()
-    hop.hint_char2 { direction = directions.AFTER_CURSOR, current_line_only = false }
+  vim.keymap.set("n", "Ö", function()
+    hop.hint_char2 { direction = directions.BEFORE_CURSOR, current_line_only = false }
   end, { remap = true })
 end
 
