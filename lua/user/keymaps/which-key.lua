f9c44b90d85c7fc65dc4c168b444684f919157e0
Pasi Bergman <pasi.bergman@hiq.fi>
2022-12-21 00:37:00 +0200

    Add mapping lR for restarting LSP clients (excl. Copilot and Null-ls)

--- lua/user/keymaps/which-key.lua
+++ lua/user/keymaps/which-key.lua
@@ -47,7 +47,21 @@ if vim.fn.executable "delta" == 1 then
   }
 end
 
-lvim.builtin.which_key.mappings["lR"] = { "<cmd>edit<cr>", "Restart LSPs" }
+local restartActiveLsp = function()
+  local clients = vim.lsp.get_active_clients()
+  for _, client in pairs(clients) do
+    if client.name ~= "copilot" and client.name ~= "null-ls" then
+      vim.schedule(function()
+        local name = client.name
+        local cmd = "LspRestart " .. client.id
+        vim.cmd(cmd)
+        print(cmd .. " (" .. name .. ") - completed.")
+      end)
+    end
+  end
+end
+
+lvim.builtin.which_key.mappings["lR"] = { restartActiveLsp, "Restart LSPs" }
 
 -- Which-key: +Sessions
 lvim.builtin.which_key.mappings["S"] = {
